f412538286f3e3de681a36cb058c12458c6aee2b
Tyson Caul <tjcaul@icloud.com>
2024-09-26 10:42:29 -0400

    CheckStyle in LanguageCodeConverter.java

--- src/main/java/org/translation/LanguageCodeConverter.java
+++ src/main/java/org/translation/LanguageCodeConverter.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ArrayList;
 
 /**
  * This class provides the service of converting language codes to their names.
